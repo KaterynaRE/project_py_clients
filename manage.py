7e9a025a7860f3642347ccec2dc2ec3a19ab6756
KaterynaRe <katyareva88@gmail.com>
2025-05-06 18:57:02 +0300

    first commit

---
+++ manage.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+from django.core.management import call_command
+from django.core.wsgi import get_wsgi_application
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    try:
+        from django.core.management import execute_from_command_line
+        #1
+        #execute_from_command_line(sys.argv)
+        #2 запускає трикутником як звичайно
+        # get_wsgi_application()
+        # call_command('runserver', 'localhost:8000')
+        #3
+
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()

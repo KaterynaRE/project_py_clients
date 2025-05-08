7cb5d33943493a60e3ec0b6357afd609b1695a00
KaterynaRe <katyareva88@gmail.com>
2025-05-08 18:10:22 +0300

    update init

---
+++ apps/core/models/__init__.py
@@ -0,0 +1,7 @@
+from .client import Client
+from .address import Address
+from .appointment import Appointment
+from .doctor import Doctor
+from .medical_history import MedicalHistory
+from .phone import Phone
+from .reception import Reception
